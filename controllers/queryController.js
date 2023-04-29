e3e6f023b7d8782b50787252d63706f017a85927
JAY PRAKASH KUMAR <84907291+JAYPRAKASH44@users.noreply.github.com>
2023-04-30 01:06:01 +0530

    Update queryController.js

--- controllers/queryController.js
+++ controllers/queryController.js
@@ -5,12 +5,24 @@ const filterQueries = async (req, res) => {
     //Write your code here for sorting & pagination
     //1) For sorting sort salary from ascending to descending order
     //2) For Pagination set limit 5 as a default limit and default page is 1
-    // Formulae to implementing pagination: (page - 1) * limit
+    // Formulae to implementing pagination: (page - 1) * limit
     // For Sorting use    .sort('salary')
+    let {page,limit} = req.query
+    
+   
+    if(!page){
+      page =1
+    }
+    if(!limit){
+      limit =5;
+    }
+    let data = await Employee.find().sort({salary:1}).limit(+limit)
+    res.json(data)
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
 
 module.exports = { filterQueries };
